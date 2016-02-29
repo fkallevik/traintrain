b5cb21febfdbd86b4e0f809f7adfab88ba86ff73
Fredrik Kallevik <fredrik@idrift.no>
2016-02-29 19:48:45 +0100

    Remove fillables from Event model

--- app/Event.php
+++ app/Event.php
@@ -7,13 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var  array [<description>]
-     */
-    protected $fillable = ['name'];
-
-    /**
      * Get the user that owns the task
      */
     public function user()
